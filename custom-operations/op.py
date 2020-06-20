515b3ade8ed545f4b3b7f639f29577f3973902f6
minhnguyen1803lqd <nhatminh1803lqd@gmail.com>
2020-06-20 21:33:40 +0700

    update

--- custom-operations/op.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-import operations as op
-
-op.extractArguments(sys.argv)
--- custom-operations/operations.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-def extractArguments(command):
-    for i in range(0, len(command)):
-        print(command[i]);
