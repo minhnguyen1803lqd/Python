50eca964f7f16cd3ed1608bafbfc08315106d797
minhnguyen1803lqd <nhatminh1803lqd@gmail.com>
2020-06-21 16:15:59 +0700

    uploadnewproject

--- auto/gt.py
+++ auto/gt.py
@@ -13,6 +13,8 @@ command = sys.argv
 
 commit = getMessage(command)
 
-os.popen("git add --all")
-os.popen("git commit -m \"" + commit + "\"")
-os.popen("git push origin master")
+print(commit)
+
+# os.popen("git add --all")
+# os.popen("git commit -m \"" + commit + "\"")
+# os.popen("git push origin master")
